82dd6c75ab3fc881e4761adfb4b8edfd2a18d105
Jason Bautista <jasoncb@cs.stanford.edu>
2017-11-20 19:06:48 -0800

    Random List App

---
+++ app/src/main/java/com/example/jason/randomlist/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.jason.randomlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] topics = {"movement", "opposite", "gravity", "heavy"};
+        ListAdapter topicsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, topics);
+        ListView topicsListView = (ListView) findViewById(R.id.topicsList);
+        topicsListView.setAdapter(topicsAdapter);
+    }
+}
