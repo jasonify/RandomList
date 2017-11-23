44819613e01503f0707751abe77b33821c4b02c9
Jason Bautista <jasoncb@cs.stanford.edu>
2017-11-22 18:10:20 -0800

    HIdesoft keyboard

--- app/src/main/java/com/example/jason/randomlist/MainActivity.java
+++ app/src/main/java/com/example/jason/randomlist/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -40,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String etText = etNewWord.getText().toString();
+                if (etText.length() == 0) {
+                    return;
+                }
                 Log.d("BUTTON", etText);
                 topics.add(etText);
                 etNewWord.setText("");
@@ -55,12 +59,22 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 int randomNumber = getRandomNuber(0, topics.size());
                 String word = topics.get(randomNumber);
-                Log.d("BUTTON", word);
-                Toast.makeText(getApplicationContext(), word, Toast.LENGTH_LONG).show();
+
                 etRandom.setText(word);
+                hideSoftKeyboard();
             }
         });
 
+    }
+
+    /**
+     * Hides the soft keyboard
+     */
+    public void hideSoftKeyboard() {
+        if(getCurrentFocus()!=null) {
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
     }
 
     int getRandomNuber(int min, int max) {
